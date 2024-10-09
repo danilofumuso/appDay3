72df3c3723eb6123bd2e8ddf98f8b6ac329efe59
danilofumuso <danilofumuso@gmail.com>
2024-10-09 19:10:21 +0200

    suggested post added!

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,20 +1,22 @@
-import { Component } from '@angular/core';
-import { Post } from '../../interfaces/ipost';
+import { Component, OnInit } from '@angular/core';
+import { iPost } from '../../interfaces/ipost';
+import { iJSONresponse } from '../../interfaces/jsonresponse';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {
-  posts: Post[] = [];
+export class HomeComponent implements OnInit {
+  //implements OnInit implementa l'interfaccia che ha come metodo ngOnInit così non si sbaglia a scriverlo!
+  posts: iPost[] = [];
   indexRandom: number = 0;
 
   ngOnInit() {
     fetch('db.json')
       .then((response) => {
         if (response.ok) {
-          return response.json();
+          return <Promise<iJSONresponse>>response.json();
         } else {
           throw new Error('Errore nella get!');
         }
@@ -22,7 +24,7 @@ export class HomeComponent {
       .then((data) => {
         this.posts = data.posts; //non faccio push ma uso l'uguale
         console.log(this.posts); //mi da l'array che mi aspettavo
-        this.indexRandom = Math.floor(Math.random() * 30); // indice random per avere un post casuale!
+        this.indexRandom = Math.floor(Math.random() * this.posts.length); // indice random per avere un post casuale!
         console.log(this.indexRandom);
       })
       .catch((err) => {
